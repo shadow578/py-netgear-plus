c2497b55959ca1ca5ffff844fb160562df2f5b0b
Michiel Fokke <github@fokke.org>
2024-10-27 17:42:45 +0000

    MVP unit test

---
+++ tests/test___init__.py
@@ -0,0 +1,15 @@
+"""Unit tests for the netgear_plus __init__ module."""
+
+
+from netgear_plus import (
+    _from_bytes_to_megabytes,
+)
+
+
+def test_from_bytes_to_megabytes() -> None:
+    """Test cases for _from_bytes_to_megabytes function."""
+    assert _from_bytes_to_megabytes(1000000) == 1.00
+    assert _from_bytes_to_megabytes(5000000) == 5.00
+    assert _from_bytes_to_megabytes(123456789) == 123.46
+    assert _from_bytes_to_megabytes(0) == 0.00
+    assert _from_bytes_to_megabytes(-1000000) == -1.00
