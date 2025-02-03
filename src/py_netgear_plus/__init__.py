a09a33f7472d89bfc7aa36a784901c5cb38d67bb
Michiel Fokke <github@fokke.org>
2025-02-03 17:15:41 +0000

    Fix workflows

--- src/py_netgear_plus/__init__.py
+++ src/py_netgear_plus/__init__.py
@@ -26,7 +26,7 @@ from .models import (
 )
 from .parsers import NetgearPlusPageParserError, create_page_parser
 
-__version__ = "0.4.3rc3"
+__version__ = "0.4.3rc4"
 
 DEFAULT_PAGE = "index.htm"
 MAX_AUTHENTICATION_FAILURES = 3
